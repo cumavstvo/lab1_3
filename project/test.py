2ee49b012c3dc3e5a7a8d9990b135df61957260d
user <user@ad.vmm.pstu.ru>
2025-09-19 10:19:00 +0500

    Добавил программу для тестирования

---
+++ project/test.py
@@ -0,0 +1,7 @@
+from lib import count_common_elements
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [7, 3, 2, 1, 9]
+list3 = [3, 2, 0, 6, 7]
+
+print("Количество общих элементов: ", count_common_elements(list1, list2,list3))
